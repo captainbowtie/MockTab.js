e9140168e2a4b4fa64ec2e73a9e4019bbc4988eb
Allen Barr <captainbowtie@users.noreply.github.com>
2018-06-15 23:43:45 -0500

    Updated modules; dropping old collections Updated npm modules to latest version Made some progress on figuring out how to drop old collections when instantiating a new database

--- db.js
+++ db.js
@@ -1,13 +1,15 @@
 const db = {
-    hostname: "localhost",
-    readUser: "readUser",
-    readPass: "readPass",
-    writeUser: "writeUser",
-    writePass: "writePass",
+	hostname: "localhost",
+	readUser: "readUser",
+	readPass: "readPass",
+	writeUser: "writeUser",
+	writePass: "writePass",
 };
 
-export const urls = {
-    read: "mongodb://"+db.readUser+":"+db.readPass+"@"+db.hostname+"/MockTab",
-    write: "mongodb://"+db.writeUser+":"+db.writePass+"@"+db.hostname+"/MockTab",
-    setup: "mongobd://"+db.hostname+"/MockTab"
+const urls = {
+	read: "mongodb://"+db.readUser+":"+db.readPass+"@"+db.hostname+"/MockTab",
+	write: "mongodb://"+db.writeUser+":"+db.writePass+"@"+db.hostname+"/MockTab",
+	setup: "mongodb://localhost/MockTab" //"mongodb://"+db.hostname+"/MockTab"
 };
+
+module.exports = urls;
--- dbSetup.js
+++ dbSetup.js
@@ -1,8 +1,51 @@
-import urls from "db.js";
-import MongoClient from "mongodb";
+const urls = require("./db.js");
+const MongoClient = require("mongodb");
 
-MongoClient.connect(urls.setup, function(err, db) {
-    if (err) throw err;
+const collections = [
+	"users",
+	"teams",
+	"competitors",
+	"buildings",
+	"rooms",
+	"judges",
+	"judgeConficts",
+	"teamConflicts",
+	"roles",
+	"userRoles",
+	"pairings",
+	"judgeUsers",
+	"coaches",
+	"ballots",
+	"status"];
 
-    db.close();
+/*Connect to database
+MongoClient.connect(urls.setup, function(err, client) {
+	//handle error if any
+	if (err) throw err;
+	
+	const db = client.db("MockTab");
+
+	//iterate through list of collections and delete the old one, then create a new one
+	for(let collectionName of collections){
+		//Delete the old collection
+		db.collection(collectionName).drop(function(err,dropOK){
+			if (err) throw err;
+
+		});
+	}
+	client.close();
 });
+*/
+for(let collectionName of collections){
+	MongoClient.connect(urls.setup, function(err, client) {
+		//handle error if any
+		if (err) throw err;
+
+		const db = client.db("MockTab");
+		db.collection(collectionName).drop(function(err,dropOK){
+			if (err) throw err;
+
+		});
+		client.close();
+	});
+}
